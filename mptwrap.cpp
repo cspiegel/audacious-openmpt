2cdcd60662ced999e7911200111161da9c781bb0
Chris Spiegel <cspiegel@gmail.com>
2019-01-30 16:58:16 -0800

    Update license information.

--- mptwrap.cpp
+++ mptwrap.cpp
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2015 Chris Spiegel
+ * Copyright (c) 2015-2019 Chris Spiegel
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
